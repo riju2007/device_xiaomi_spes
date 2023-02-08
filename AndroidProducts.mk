37d29c737aeba7235599d01224ce86de9fb4d8c7
Bratasij Chakraborty <back.bratasij@gmail.com>
2023-02-08 16:04:43 +0530

    BananaDroid Bringup and Add-ons

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_spes.mk
+    $(LOCAL_DIR)/banana_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_spes-user \
-    blaze_spes-userdebug \
-    blaze_spes-eng
+    banana_spes-user \
+    banana_spes-userdebug \
+    banana_spes-eng
--- banana_spes.mk
+++ banana_spes.mk
@@ -11,11 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Inherit some common BananaDroid stuff
+$(call inherit-product, vendor/banana/config/common.mk)
+BANANA_MAINTAINER := Bratasij
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+
+#Gapps
+WITH_GAPPS := true
+
 
 # Product Specifics
-PRODUCT_NAME := blaze_spes
+PRODUCT_NAME := banana_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -24,15 +31,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Blaze Official Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := sayann70
-TARGET_FACE_UNLOCK_SUPPORTED := true
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-banana
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
